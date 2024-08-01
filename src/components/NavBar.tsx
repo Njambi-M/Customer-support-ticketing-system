5ad7f05fa17d367afd5dcb51304ab0fb74e1dd99
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-08-01 17:26:46 +0300

    style: change colour of menu icon

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -15,7 +15,7 @@ export default function NavBar() {
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             <button
               type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              className="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-slate-950 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-controls="mobile-menu"
               aria-expanded="false"
               onClick={toggleMenu}
