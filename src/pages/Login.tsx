46a6ab887541f79e1676c037a6deb3cc3c5776e4
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-08-04 22:55:21 +0300

    feat: add introduction text to login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 
 export default function Login(){
     return(
-        
+        <div>
+            <div className="login"> 
+                <h1>Sign into your account</h1>
+                <p>If you are new, {" "}</p>
+                {/* <Link href="">sign up</Link> */}
+                
+
+            </div>
+        </div>
     );
 }
