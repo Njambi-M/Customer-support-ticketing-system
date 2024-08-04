f1b74821fde8c47c757c5852ffc21cc0849a65d4
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-08-04 23:00:26 +0300

    feat: create a form schema

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,6 +3,13 @@ import React from 'react';
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 
+import { z } from "zod"
+ 
+const formSchema = z.object({
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z.string().min(8, { message: "Must be more than 8 characters" }),
+})
+
 
 export default function Login(){
     return(
