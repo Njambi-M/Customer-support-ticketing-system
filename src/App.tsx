f39cb5284ffef54211b32ec903c7273b8dc736a9
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-08-05 14:38:44 +0300

    feat: add routing for pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar'
 import './styles/globals.css'
+import AppRouter from './Router';
 
 function App() {
   return (
     <div className="App">
+      <AppRouter/>
        <NavBar/>
     </div>
   );
---
+++ src/Router.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+
+const AppRouter: React.FC = () => ( 
+    <Router>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </Router>
+);
+
+export default AppRouter;
