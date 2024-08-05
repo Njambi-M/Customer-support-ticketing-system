76fb72a58e8894eb6ca791c406f1dcbfd0efff66
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-08-05 13:24:07 +0300

    feat: build the form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,43 +1,86 @@
-"use client"
-import React from 'react';
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+"use client";
+import React from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
-import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
- 
 const formSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
   password: z.string().min(8, { message: "Must be more than 8 characters" }),
-})
+});
 
-
-export default function Login(){
-    // 1. Define the form.
+export default function Login() {
+  // 1. Define the form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
-      password:  "",
+      password: "",
     },
-  })
+  });
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    console.log(values);
   }
-    return(
-        <div>
-            <div className="login"> 
-                <h1>Sign into your account</h1>
-                <p>If you are new, {" "}</p>
-                {/* <Link href="">sign up</Link> */}
-                
-
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="login">
+        <h1>Sign into your account</h1>
+        <p>If you are new, </p>
+        {/* <Link href="">sign up</Link> */}
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your email address" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+                control={form.control}
+                name="password"
+                render={({field}) => (
+                    <FormItem>
+                        <FormLabel>
+                            <FormControl>
+                                <Input 
+                                type="password"
+                                placeholder="Enter password"
+                                {...field}
+                                />
+                            </FormControl>
+                        </FormLabel>
+                    </FormItem>
+                )}
+                />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
 }
