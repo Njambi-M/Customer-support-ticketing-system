67ab5e31ee5f321c948fe06d89b09ba68db4a93a
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-08-08 15:03:11 +0300

    feat: add more elements to the login form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -9,9 +9,11 @@ import {
   FormControl,
   FormField,
   FormItem,
+  FormMessage,
   FormLabel,
 } from "../components/ui/form";
 import { Input } from "../components/ui/input";
+import {Link} from 'react-router-dom';
 
 const formSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
@@ -38,8 +40,7 @@ export default function Login() {
     <div>
       <div className="login">
         <h1>Sign into your account</h1>
-        <p>If you are new, </p>
-        {/* <Link href="">sign up</Link> */}
+        <p>If you are new, <Link to ="/register" className="font-semibold">sign up</Link> </p><br/>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <FormField
@@ -47,10 +48,11 @@ export default function Login() {
               name="email"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Username</FormLabel>
+                  <FormLabel>Username:</FormLabel>
                   <FormControl>
                     <Input placeholder="Enter your email address" {...field} />
                   </FormControl>
+                  <FormMessage/>
                 </FormItem>
               )}
             />
@@ -59,7 +61,7 @@ export default function Login() {
                 name="password"
                 render={({field}) => (
                     <FormItem>
-                        <FormLabel>
+                        <FormLabel>Password:</FormLabel>
                             <FormControl>
                                 <Input 
                                 type="password"
@@ -67,14 +69,49 @@ export default function Login() {
                                 {...field}
                                 />
                             </FormControl>
-                        </FormLabel>
+                            <FormMessage/>
                     </FormItem>
                 )}
                 />
-            <Button type="submit">Submit</Button>
+            <Button type="submit">Sign In</Button>
+            <div className="flex items-center mb-4">
+              <div className="flex-grow h-px bg-gray-300" />
+              <span className=" text-sm text-gray-500 ">or continue with</span>
+              <div className="flex-grow h-px bg-gray-300" />
+            </div>
+            <div className="flex justify-center">
+              <Button>
+            <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  x="0px"
+                  y="0px"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 48 48"
+                >
+                  <path
+                    fill="#fbc02d"
+                    d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12	s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20	s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+                  ></path>
+                  <path
+                    fill="#e53935"
+                    d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039	l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+                  ></path>
+                  <path
+                    fill="#4caf50"
+                    d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+                  ></path>
+                  <path
+                    fill="#1565c0"
+                    d="M43.611,20.083L43.595,20L42,20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+                  ></path>
+                </svg>
+                <span> Google</span>
+                </Button>
+              </div>
           </form>
         </Form>
       </div>
-    </div>
+      </div>
   );
 }
