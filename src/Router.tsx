f20512b2fdd018291f35ab4f0b63eadfdfbe9012
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-08-08 18:59:43 +0300

    feat: add contact page to the routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Contact from './pages/Contact';
 
 const AppRouter: React.FC = () => ( 
     <Router>
     <Routes>
       <Route path="/login" element={<Login/>} />
       <Route path="/register" element={<Register/>} />
+      <Route path="/contact" element={<Contact/>} />
     </Routes>
   </Router>
 );
