bddf02bf594bbb9a474945738892da945cdccbe3
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-08-08 19:12:17 +0300

    build: add shadcn components

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,4 +1,17 @@
 import React from 'react';
+import { Textarea } from "@/components/ui/textarea";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../components/ui/form";
+import { Input } from "../components/ui/input";
+import { useState } from "react";
+import { useToast } from "@/components/ui/use-toast";
 
 export default function Contact(){
     
