fe19e42b7d61938a50eb6bf9cbcfe0fc5ef6f030
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-08-08 19:38:50 +0300

    fix: change the imports of the shadcn components

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,7 +3,7 @@
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { Textarea } from "@/components/ui/textarea";
+import { Textarea } from "../components/ui/textarea";
 
 import {
   Form,
