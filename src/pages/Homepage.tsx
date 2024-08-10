8bbe32ea166c87913c662987b208c664b39594a0
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-08-10 18:17:57 +0300

    feat: add call to action button

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Button } from "../components/ui/button";
+import { Link } from "react-router-dom";
 
-export default function Homepage(){
-    return(
-        <div>
-            <div>
-                <h1>Welcome to Customer Ticketing</h1>
-                <p>Raise a ticket and have your issue solved within minutes</p>
-            </div>
-            <div>
-              <img src="/ticket.png"/>  
-            </div>
-        </div>
-    );
+export default function Homepage() {
+  return (
+    <div>
+      <div>
+        <h1>Welcome to Customer Ticketing</h1>
+        <p>Raise a ticket and have your issue solved within minutes</p>
+        <Button asChild>
+          <Link to="/register">Get started</Link>
+        </Button>
+      </div>
+      <div>
+        <img src="/ticket.png" />
+      </div>
+    </div>
+  );
 }
