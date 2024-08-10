d7148b1ccc0a1e7a5c39a2a6c89628ade7dadcef
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-08-10 15:58:31 +0300

    feat: add base of the homepage

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Homepage(){
+    return(
+        <div>
+            <div>
+                <h1>Welcome to Customer Ticketing</h1>
+                <p>Raise a ticket and have your issue solved within minutes</p>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    );
+}
