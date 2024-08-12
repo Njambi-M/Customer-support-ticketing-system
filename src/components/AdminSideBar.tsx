6ba95047b2e734b40ef6641baf796190cad3fe08
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-08-12 10:36:49 +0300

    feat: add to the side bar

--- src/components/AdminSideBar.tsx
+++ src/components/AdminSideBar.tsx
@@ -1,9 +1,34 @@
-"use client"
+"use client";
 import { Link } from "react-router-dom";
 
-
-export default function AdminSideBar(){
-    return(
-
-    );
+export default function AdminSideBar() {
+  return (
+    <div>
+      <aside className="hidden md:block w-64 bg-white text-black h-screen p-6 border-r border-gray-200">
+        <div className="mb-8 text-sm">
+          <h2 className="font-semibold mb-4">Hello: </h2>
+          <nav className="flex flex-col space-y-2">
+            <Link
+              to="#"
+              className="flex items-center gap-5 text-muted-foreground transition-colors hover:text-primary p-2"
+            >
+              View all tickets
+            </Link>
+            <Link
+              to="#"
+              className="flex items-center gap-5 text-muted-foreground transition-colors hover:text-primary p-2"
+            >
+              Solved tickets
+            </Link>
+            <Link
+              to="#"
+              className="flex items-center gap-5 text-muted-foreground transition-colors hover:text-primary p-2"
+            >
+              Unsolved tickets
+            </Link>
+          </nav>
+        </div>
+      </aside>
+    </div>
+  );
 }
